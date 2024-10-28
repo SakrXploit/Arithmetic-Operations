ec7ab996c9669400cd1e32e6b8745050f601c03d
SakrXploit <omarsakr262@gmail.com>
2024-10-28 18:47:10 +0300

    initial commit

---
+++ Area of a Rectangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+  int L,W;
+  int Area ;
+  cout<<"Enter length\n";
+  cin>>L;
+  cout<<"Enter width\n";
+  cin>>W;
+  Area=  L * W;
+  cout<<"Area = "<< Area <<endl;
+
+  
+  return 0;
+}
